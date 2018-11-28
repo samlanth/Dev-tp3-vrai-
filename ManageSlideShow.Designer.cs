8733eb5af48e53100bdb596bf3d644ac3193a993
samlanth <samuellanthier@hotmail.com>
2018-11-28 17:13:41 -0500

    Q

---
+++ ManageSlideShow.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Client_PM
+{
+    partial class ManageSlideShow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FLPN_Photos = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // FLPN_Photos
+            // 
+            this.FLPN_Photos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FLPN_Photos.Location = new System.Drawing.Point(12, 12);
+            this.FLPN_Photos.Name = "FLPN_Photos";
+            this.FLPN_Photos.Size = new System.Drawing.Size(445, 416);
+            this.FLPN_Photos.TabIndex = 0;
+            // 
+            // ManageSlideShow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 440);
+            this.Controls.Add(this.FLPN_Photos);
+            this.Name = "ManageSlideShow";
+            this.Text = "ManageSlideShow";
+            this.Shown += new System.EventHandler(this.ManageSlideShow_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel FLPN_Photos;
+    }
+}
---
+++ Slideshow.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PhotoManagerClient;
+
+namespace Client_PM
+{
+    public partial class Slideshow : Form
+    {
+        private List<Photo> photos;
+        private List<int> SlideShowList;
+        public Slideshow()
+        {
+            InitializeComponent();
+        }
+        private void Load_Settings()
+        {
+            if (!Properties.Settings.Default.First_Execution)
+            {
+                LoadSlideShowList();
+            }
+            else
+            {
+                SlideShowList = new List<int>();
+            }
+        }
+
+        private void Save_settings()
+        {
+            Properties.Settings.Default.First_Execution = false;
+            SaveSlideShowList();
+            Properties.Settings.Default.Save();
+        }
+        private void LoadSlideShowList()
+        {
+            SlideShowList = new List<int>();
+            if (Properties.Settings.Default.SlideShowList != null)
+            {
+                foreach (string stringPhotoId in Properties.Settings.Default.SlideShowList)
+                {
+                    int photoId = int.Parse(stringPhotoId);
+                    SlideShowList.Add(photoId);
+                }
+            }
+        }
+
+        private void SaveSlideShowList()
+        {
+            if (Properties.Settings.Default.SlideShowList == null)
+                Properties.Settings.Default.SlideShowList = new System.Collections.Specialized.StringCollection();
+            Properties.Settings.Default.SlideShowList.Clear();
+            if (SlideShowList != null)
+                foreach (int photoId in SlideShowList)
+                {
+                    Properties.Settings.Default.SlideShowList.Add(photoId.ToString());
+                }
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            WaitSplash.Show(this, "Loading photos from server...");
+            photos = DBPhotosWebServices.GetAllPhotos();
+            //photos = DBPhotosWebServices.GetAllPhotos().Where(p=>BlacklistedUsers.indexof(p.ownerid)==-1).tolist();
+            WaitSplash.Hide();
+        }
+
+        private void Slideshow_Load(object sender, EventArgs e)
+        {
+            Load_Settings();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ManageSlideShow dlg = new ManageSlideShow();
+                dlg.SlideShowList = SlideShowList;
+                dlg.PhotoPool = photos;
+                dlg.ShowDialog();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("lag");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Diaporama dlg = new Diaporama();
+            dlg.SlideShowList = SlideShowList;
+            dlg.PhotoPool = photos;
+            dlg.ShowDialog();
+        }
+
+        private void Slideshow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Save_settings();
+        }
+    }
+}
